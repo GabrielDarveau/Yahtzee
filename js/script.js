81969e659c72df0a84aa3cec6b68e83163cf9c1c
NathanMorasse <2133133@etu.cchic.ca>
2022-11-24 10:06:43 -0500

    cacher dés

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 function innit(){
-    getElementById("1").style.display = "none";
-    getElementById("2").style.display = "none";
-    getElementById("3").style.display = "none";
-    getElementById("4").style.display = "none";
-    getElementById("5").style.display = "none";
+
 }
 
 function Lancer(){
     var nbDes = GetNombreDes();
+
 }
 
 function GetNombreDes(){
     var nbDes = 0;
     for(let i = 0; i < 5; i++){
-        if(document.getElementById(i).visibility != hidden){
+        if(document.getElementById(i).style.display == block){
             nbDes++;
         }
     }
